1fe573fb16ad556a9fb184b0883a0c994874e8bb
rakeshrgit <rakesh2abhishek@gmail.com>
2022-07-26 20:59:13 +0530

    added

--- src/components/pagination.jsx
+++ src/components/pagination.jsx
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import lodash from 'lodash';
 const Pagination = (props) => {
     const {itemsCount, pageSize, currentPage, onPageChanges} = props;
-    console.log('currentPage', currentPage)
+    //console.log('currentPage', currentPage)
     const pageCount = Math.ceil(itemsCount/pageSize);
     const pages = lodash.range(1, pageCount + 1);
     //console.log('pageCount', pageCount)
