3f3b91f043b9baffcdc3284766dbfbe42f2b51b0
rakeshrgit <rakesh2abhishek@gmail.com>
2023-01-07 12:49:58 +0530

    changes added

--- src/App.js
+++ src/App.js
@@ -3,12 +3,18 @@ import './App.css';
 import UseMemoFunction from './usememo/memoFunction';
 import CallBackFunction from './usecallback/callbackFunction';
 import Title from './usecallback/Title';
+import Hero from './errorBoundry/Hero';
+import ErrorBoundry from './errorBoundry/ErrorBoundry';
 
 function App() {
   return (
     <div className="App">
-      <Title/>
-      <CallBackFunction/>
+       <h1>Error Boundary</h1>
+       <ErrorBoundry>
+       <Hero heroName="Ram"></Hero>
+       <Hero heroName="Superman"></Hero>
+       <Hero heroName="Joker"></Hero>
+       </ErrorBoundry>
     </div>
     
   );
